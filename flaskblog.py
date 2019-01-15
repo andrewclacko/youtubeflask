d92e8ea479bd7bb94903313697f82a4ade733872
Andy Clark <clacko@GB5CG6216T0V.CSCMWS.CSCMWS.COM>
2019-01-15 21:26:39 +0000

    updated html

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello World! also</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>About this page</h1>"
+    return "<h1>About</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
