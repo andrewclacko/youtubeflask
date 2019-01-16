081a6f9422df90e4b2be1810b609ba00d2b1bb54
Andy Clark <clacko@GB5CG6216T0V.CSCMWS.CSCMWS.COM>
2019-01-16 23:00:51 +0000

    html and css added and works

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Andrew Clark',
+        'title': 'Blog Post 1',
+        'content': 'This is my first post',
+        'date_posted': 'January 15, 2019'  
+    },
+    {
+        'author': 'Lewis Clark',
+        'title': 'Blog Post 2',
+        'content': 'This is my second post',
+        'date_posted': 'January 16, 2019' 
+    },
+        {
+        'author': 'Kelly Clark',
+        'title': 'Blog Post 4',
+        'content': 'This is my fourth post',
+        'date_posted': 'January 10, 2019' 
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
